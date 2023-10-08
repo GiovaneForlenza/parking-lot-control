14fe69a8428040b8e84c3230e35f19ddfd4f8df5
Giovane Forlenza <giovane.franciscoforl@gmail.com>
2023-10-07 21:07:02 -0300

    Started to work on error handling

--- src/pages/CheckIn.js
+++ src/pages/CheckIn.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 
 import getSortedCarMakeList, {
   getListOfCarModels,
@@ -18,6 +18,13 @@ function CheckIn() {
     carModelsFromMake,
     carColorList,
   } = useContext(CarContext);
+
+  const [errorChecking, setErrorChecking] = useState({
+    plate: false,
+    make: false,
+    model: false,
+    color: false,
+  });
 
   useEffect(() => {
     fetchAllCarModels();
@@ -44,17 +51,18 @@ function CheckIn() {
   };
 
   const handleButtonClick = () => {
-    if (
-      car.make === "" ||
-      car.plate === "" ||
-      car.model === "" ||
-      car.color === ""
-    ) {
-      console.log("FALSE");
-    } else {
-      console.log("true");
-    }
-    console.log(car)
+    setErrorChecking({
+      plate: false,
+      make: false,
+      model: false,
+      color: false,
+    });
+
+    setErrorChecking({ ...errorChecking, plate: car.plate === "" });
+    setErrorChecking({ ...errorChecking, make: car.make === "" });
+    setErrorChecking({ ...errorChecking, model: car.model === "" });
+    setErrorChecking({ ...errorChecking, color: car.color === "" });
+    console.log(errorChecking)
   };
 
   return (
@@ -69,100 +77,144 @@ function CheckIn() {
             </div>
 
             {/* Car Plate */}
-            <div className="user-input-wrp">
-              <br />
-              <input
-                type="text"
-                className="inputText px-1 border-2"
-                required
-                onChange={(event) => {
-                  handlePlateUpdate(event);
-                }}
-              />
-              <span className="floating-label">Car Plate</span>
+            <div className="">
+              {/* INPUT */}
+              <div className="user-input-wrp">
+                <br />
+                <input
+                  type="text"
+                  className="inputText px-1 border-2"
+                  required
+                  onChange={(event) => {
+                    handlePlateUpdate(event);
+                  }}
+                />
+                <span className="floating-label">Car Plate</span>
+              </div>
+              {/* ERROR */}
+              <div
+                className={`text-sm text-red-600 font-semibold ${
+                  errorChecking.plate ? "inline-block" : "hidden"
+                }`}
+              >
+                The car plate is a required field!
+              </div>
             </div>
             {/* Car Make */}
-            <div className="mt-8 flex pb-2 justify-center items-center">
-              <label htmlFor="" className=" w-28">
-                Car Make
-              </label>
-              <select
-                name=""
-                id=""
-                className="w-full p-1 select-none"
-                onChange={(event) => handleChangeCarMakeSelection(event)}
+            <div className="">
+              {/* SELECT */}
+              <div className="mt-8 flex pb-2 justify-center items-center">
+                <label htmlFor="" className=" w-28">
+                  Car Make
+                </label>
+                <select
+                  name=""
+                  id=""
+                  className="w-full p-1 select-none"
+                  onChange={(event) => handleChangeCarMakeSelection(event)}
+                >
+                  <option value="">Select a car make</option>
+                  {carMakers.map((make) => {
+                    return (
+                      <option value={make} key={make}>
+                        {make}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+              {/* ERROR */}
+              <div
+                className={`text-sm text-red-600 font-semibold ${
+                  errorChecking.make === true ? "block" : "hidden"
+                }`}
               >
-                <option value="">Select a car make</option>
-                {carMakers.map((make) => {
-                  return (
-                    <option value={make} key={make}>
-                      {make}
-                    </option>
-                  );
-                })}
-              </select>
+                The car make is a required field!
+              </div>
             </div>
             {/* Car Model, Color and Button conditional */}
             {car.make !== "" ? (
               <div className="">
                 {/* Car Model */}
-                <div className="mt-8 flex pb-2 justify-center items-center">
-                  <label htmlFor="carModels" className="w-28">
-                    Car Model
-                  </label>
-                  <select
-                    name="carModels"
-                    id="carModels"
-                    className="w-full p-1 select-none"
-                    onChange={(event) => {
-                      handleChangeCarModelSelection(event);
-                    }}
+                <div className="">
+                  {/* SELECT */}
+                  <div className="mt-8 flex pb-2 justify-center items-center">
+                    <label htmlFor="carModels" className="w-28">
+                      Car Model
+                    </label>
+                    <select
+                      name="carModels"
+                      id="carModels"
+                      className="w-full p-1 select-none"
+                      onChange={(event) => {
+                        handleChangeCarModelSelection(event);
+                      }}
+                    >
+                      <option value="">Select a car Model</option>
+                      {carModelsFromMake.map((model) => {
+                        return (
+                          <option value={model} key={model}>
+                            {model}
+                          </option>
+                        );
+                      })}
+                    </select>
+                  </div>
+                  {/* ERROR */}
+                  <div
+                    className={`text-sm text-red-600 font-semibold ${
+                      errorChecking.model === true ? "block" : "hidden"
+                    }`}
                   >
-                    <option value="">Select a car Model</option>
-                    {carModelsFromMake.map((model) => {
-                      return (
-                        <option value={model} key={model}>
-                          {model}
-                        </option>
-                      );
-                    })}
-                  </select>
+                    The car model is a required field!
+                  </div>
+                  {/* SPAN */}
+                  <div className="text-xs text-slate-400 select-none">
+                    *Not all car models are available, this project is using a
+                    demo API
+                  </div>
                 </div>
-                <div className="text-xs text-slate-400 select-none">
-                  *Not all car models are available, this project is using a
-                  demo API
-                </div>
-
                 {/* Color */}
-                <div className="mt-8 flex pb-2 justify-center items-center">
-                  <div className="w-28">Color</div>
-                  <div className="w-full  grid grid-cols-5 grid-rows-2 gap-2">
-                    {carColorList.map((color) => {
-                      return (
-                        <div
-                          className={`rounded-sm flex flex-col items-center transition-all hover:cursor-pointer hover:scale-105 ${
-                            car.color === color.name ? "shadow-lg" : ""
-                          }`}
-                          key={color.hex}
-                          onClick={() => {
-                            handleColorClick(color.name);
-                          }}
-                        >
+                <div className="">
+                  {/* COLOR SELECTION */}
+                  <div className="mt-8 flex pb-2 justify-center items-center">
+                    <div className="w-28">Color</div>
+                    <div className="w-full  grid grid-cols-5 grid-rows-2 gap-2">
+                      {carColorList.map((color) => {
+                        return (
                           <div
-                            className={`w-8 h-8  rounded-full bg-[#${
-                              color.hex
-                            }] shadow-sm border ${
-                              car.color === color.name
-                                ? "border-[3px] border-blue-800"
-                                : ""
+                            className={`rounded-sm flex flex-col items-center transition-all hover:cursor-pointer hover:scale-105 ${
+                              car.color === color.name ? "shadow-lg" : ""
                             }`}
-                          ></div>
-                          <div className="text-sm select-none">
-                            {color.name}
+                            key={color.hex}
+                            onClick={() => {
+                              handleColorClick(color.name);
+                            }}
+                          >
+                            <div
+                              className={`w-8 h-8  rounded-full bg-[#${
+                                color.hex
+                              }] shadow-sm border ${
+                                car.color === color.name
+                                  ? "border-[3px] border-blue-800"
+                                  : ""
+                              }`}
+                            ></div>
+                            <div className="text-sm select-none">
+                              {color.name}
+                            </div>
                           </div>
-                        </div>
-                      );
-                    })}
+                        );
+                      })}
+                    </div>
+                  </div>
+                  {/* ERROR */}
+                  <div
+                    className={`text-sm text-red-600 font-semibold ${
+                      errorChecking.color === true ? "block" : "hidden"
+                    }`}
+                  >
+                    The car color is a required field!
                   </div>
                 </div>
 
